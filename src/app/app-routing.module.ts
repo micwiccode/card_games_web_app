45302e3020b35b3f0ac8ce6005c7e6a24b31f55b
Michał <m.wiczk@wp.pl>
2020-04-10 23:14:26 +0200

    Add test version of main view and header component

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,11 +1,15 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { MainPageComponent } from './main-page/main-page.component';
 
 
-const routes: Routes = [];
+const appRoutes: Routes = [
+  { path: '', component: MainPageComponent }
+];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  imports: [RouterModule.forRoot(appRoutes, { enableTracing: true })],
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
+
