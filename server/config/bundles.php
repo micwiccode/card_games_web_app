38a14ee2a3c94deb3d9dd7e1b8b438c2ed1b25ca
Maciek Bartosik <maciek@bartosik.wtf>
2020-05-23 12:27:16 +0200

    Add token auth:

--- server/config/bundles.php
+++ server/config/bundles.php
@@ -12,4 +12,5 @@ return [
     Symfony\Bundle\WebProfilerBundle\WebProfilerBundle::class => ['dev' => true, 'test' => true],
     Symfony\Bundle\MercureBundle\MercureBundle::class => ['all' => true],
     Nelmio\CorsBundle\NelmioCorsBundle::class => ['all' => true],
+    Lexik\Bundle\JWTAuthenticationBundle\LexikJWTAuthenticationBundle::class => ['all' => true],
 ];
